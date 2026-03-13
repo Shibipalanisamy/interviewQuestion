c8b2118e15f9b5548c173f6f0a46f5d2fe4bbe52
kkpsh <kkpshibi@gmail.com>
2026-03-13 19:57:03 +0530

    Add project files.

---
+++ Partices/Basic/yieldPartices.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.VisualBasic;
+
+namespace Partices.Basic
+{
+    public class yieldPartices
+    {
+
+        public void printallCountry()
+        {
+            yieldPartices partices = new yieldPartices();
+            foreach (string part in partices.withoutYieldprintCountry("u"))
+            {
+                Console.WriteLine(part);
+            }
+        }
+        public IEnumerable<string> withoutYieldprintCountry(string country)
+        {
+            List<string> resultList = new List<string>();
+            List<string> AllpoductCountryList = new List<string>() { "UK", "US", "Europ", "India" };
+            for (int i = 0; i < AllpoductCountryList.Count; i++)
+            {
+                if (AllpoductCountryList[i].ToLower().Contains(country))
+                {
+                    Console.WriteLine(AllpoductCountryList[i]);
+                    resultList.Add(AllpoductCountryList[i]);
+
+                }
+            }
+            return resultList;
+        }
+        public IEnumerable<string> withYieldprintCountry(string ch)
+        {
+            List<string> ll = new List<string>() { "UK", "US", "India", "Europ" };
+            for (int i = 0; i < ll.Count; i++)
+            {
+                {
+                    if (ll[i].ToLower().Contains(ch))
+                    {
+                        yield return ll[i];
+                    }
+                }
+            }
+        }
+    }
+
+    class Animal : iAnimal
+    {
+        public void sleep()
+        {
+
+        }
+        public void Eat() {
+            Console.WriteLine("Animal will eat");
+        }
+    }
+    class Dog:Animal
+    {
+        public void bark()
+        {
+            Console.WriteLine("Dog will bark");
+        }
+        
+    }
+    interface iAnimal
+    {
+       const int i=10;
+        void sleep();
+    }
+}
---
+++ Partices/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Partices.Basic;
+namespace Partices
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            yieldPartices yo = new yieldPartices();
+            yo.printallCountry();
+
+            Animal animal= new Animal();
+            animal.Eat();
+            Dog dog= new Dog();
+            dog.Eat();
+            dog.bark();
+            Animal an = new Dog();
+            an.Eat();
+            iAnimal animal1 = new Dog();
+            animal1.sleep();
+
+            
+
+        }
+    }
+}
